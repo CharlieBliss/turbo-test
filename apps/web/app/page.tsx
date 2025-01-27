a4243ab71cc19a1d97195aa547a5f289efbacb38
Charles Bliss <charles.m.bliss@gmail.com>
2025-01-26 20:41:20 -0500

    Turbo??

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,10 @@
 import { prisma } from "@repo/database";
 
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
-
+  console.log(prisma._engineConfig)
   return (
     <div>
       <h1>Hello World</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   );
 }
