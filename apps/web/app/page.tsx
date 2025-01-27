647f0171fb58dcb117108310f9c92eb284045700
Charles Bliss <charles.m.bliss@gmail.com>
2025-01-26 21:39:38 -0500

    Use client

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,17 @@
-import { prisma } from "@repo/database";
+"use client"
 
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
   let userFetch = await fetch(process.env.BASE_URL + "/api/users");
   let usersJson
   if(!userFetch.ok) {
     usersJson = await userFetch.json();
-  console.log(usersJson)
+    console.log("PAGE LEVEL", usersJson)
   } else {
     usersJson = {body: []}
   }
   return (
     <div>
       <h1>Hello World</h1>
-      {users.map((user) => (
-        <div key={user.id}>{user.id}</div>
-      ))}
       {usersJson.body.map((jUser: any) => (
         <div key={jUser.id}>{jUser.name}</div>
       ))}
