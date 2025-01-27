e5a69795f5f31719fb30caba418e40a3d4cb949f
Charles Bliss <charles.m.bliss@gmail.com>
2025-01-26 21:03:25 -0500

    BW database

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,13 @@
-'use client'
 import { prisma } from "@repo/database";
 
 export default async function IndexPage() {
-  console.log(prisma)
-
+  const users = await prisma.user.findMany();
   return (
     <div>
       <h1>Hello World</h1>
+      {users.map((user) => (
+        <div key={user.id}>{user.id}</div>
+      ))}
     </div>
   );
 }
